e2f0e7afc31e413101ca798c0df395dafbd24e58
Diego F. Pajarito Grajales <diegopajarito@gmail.com>
2017-09-12 14:38:35 +0200

    Working example of Location Aware Application based on the example here:  https://developer.android.com/training/location/retrieve-current.html  Added Location Request for continuous updates

--- app/src/main/java/eu/geoc/uji/esr7/locationtracker/MainActivity.java
+++ app/src/main/java/eu/geoc/uji/esr7/locationtracker/MainActivity.java
@@ -1,7 +1,7 @@
 package eu.geoc.uji.esr7.locationtracker;
 
 import android.Manifest;
-import android.app.Activity;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
@@ -15,9 +15,21 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.ResolvableApiException;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -25,8 +37,11 @@ public class MainActivity extends AppCompatActivity {
     private final int LOCATION_PERMISSION_CODE = 5432;
 
     private FusedLocationProviderClient mFusedLocationClient;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
 
     private TextView mTextMessage;
+    private int counter = 0;
 
     private String locationText;
 
@@ -65,11 +80,87 @@ public class MainActivity extends AppCompatActivity {
 
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
+
         checkLocationPermission();
 
-        getLocation();
+        getStaticLocation();
+
+        setUpLocationUpdateSettings();
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startLocationUpdates();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    /**
+     * This method checks if the permission(s) were already given or
+     * request for them in run-time
+     */
+
+
+    private void checkLocationPermission() {
+
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                Snackbar.make(
+                        findViewById(android.R.id.content),
+                        "The location permission is needed to provide full functionality of the app",
+                        Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Grant Permission", new View.OnClickListener() {
+                            @Override
+                            public void onClick(View view) {
+                                // Request permission
+
+                                ActivityCompat.requestPermissions(MainActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        LOCATION_PERMISSION_CODE);
+                            }
+                        })
+                        .show();
+
+
+            } else {
+
+                // No explanation needed, we can request the permission.
+
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        LOCATION_PERMISSION_CODE);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        }
+
+    }
+
+
+    /**
+     * This method executes after changes on permissions
+     * @param requestCode
+     * @param permissions
+     * @param grantResults
+     */
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
@@ -79,7 +170,9 @@ public class MainActivity extends AppCompatActivity {
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
-                    getLocation();
+                    getStaticLocation();
+
+
 
 
                 } else {
@@ -96,10 +189,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void getLocation() {
+    /**
+     * This method gets the last location know by the
+     * Google Play Services, it is static and does not update after
+     */
+
+    private void getStaticLocation() {
 
 
-        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_DENIED) {
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_DENIED) {
             mFusedLocationClient.getLastLocation()
                     .addOnSuccessListener(this, new OnSuccessListener<Location>() {
                         @Override
@@ -107,66 +205,23 @@ public class MainActivity extends AppCompatActivity {
                             // Got last known location. In some rare situations this can be null.
                             if (location != null) {
                                 locationText = location.toString();
-                                updateLocationOnScreen();
+                                setStaticLocationOnScreen();
                             }
                         }
                     });
         } else {
             locationText = "Unable to get Location - No permission granted";
-            updateLocationOnScreen();
+            setStaticLocationOnScreen();
         }
 
 
     }
 
-    private void checkLocationPermission() {
+    /**
+     * This method updates the text view for last location on screen
+     */
 
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
-
-            // Should we show an explanation?
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
-
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
-
-                Snackbar.make(
-                        findViewById(android.R.id.content),
-                        "The location permission is needed to provide full functionality of the app",
-                        Snackbar.LENGTH_INDEFINITE)
-                        .setAction("Grant Permission", new View.OnClickListener() {
-                            @Override
-                            public void onClick(View view) {
-                                // Request permission
-
-                                ActivityCompat.requestPermissions(MainActivity.this,
-                                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                                        LOCATION_PERMISSION_CODE);
-                            }
-                        })
-                        .show();
-
-
-            } else {
-
-                // No explanation needed, we can request the permission.
-
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                        LOCATION_PERMISSION_CODE);
-
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
-            }
-        }
-
-    }
-
-    private void updateLocationOnScreen(){
+    private void setStaticLocationOnScreen(){
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
@@ -176,6 +231,132 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+
+
+    /**
+     * This method defines the location request with time intervals
+     * and priority
+     */
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(10000);
+        mLocationRequest.setFastestInterval(5000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    /**
+     * This method adds a task to the current location settings of the device
+     * then it checks if location settings are satisfied to either start or not
+     * the client
+     */
+
+    protected void setUpLocationUpdateSettings(){
+
+        // Creates the location request
+        createLocationRequest();
+
+        // Setting location request
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder()
+                .addLocationRequest(mLocationRequest);
+
+        // Checking if the current location settings are satisfied
+        final SettingsClient client = LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        // If the settings are satisfied the client can start
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                // All location settings are satisfied. The client can initialize
+                // location requests here.
+                // ...
+
+                startLocationUpdates();
+
+            }
+        });
+
+        // If the settings are not satisfied some actions are taken
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                int statusCode = ((ApiException) e).getStatusCode();
+                switch (statusCode) {
+                    case CommonStatusCodes.RESOLUTION_REQUIRED:
+                        // Location settings are not satisfied, but this can be fixed
+                        // by showing the user a dialog.
+                        try {
+                            // Show the dialog by calling startResolutionForResult(),
+                            // and check the result in onActivityResult().
+                            ResolvableApiException resolvable = (ResolvableApiException) e;
+                            resolvable.startResolutionForResult(MainActivity.this,
+                                    LOCATION_PERMISSION_CODE);
+                        } catch (IntentSender.SendIntentException sendEx) {
+                            // Ignore the error.
+                            locationText = "Error - We tried to fix it; however, we could not fix it.";
+                        }
+                        break;
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        // Location settings are not satisfied. However, we have no way
+                        // to fix the settings so we won't show the dialog.
+                        locationText = "Error - It cannot be solved";
+                        break;
+                }
+            }
+        });
+    }
+
+
+
+    private void startLocationUpdates() {
+
+        mLocationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                for (Location location : locationResult.getLocations()) {
+                    // Update UI with location data
+                    // ...
+                    locationText = location.toString();
+                    updateLocationOnScreen();
+                }
+            };
+        };
+
+        if (ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_DENIED) {
+            mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, null);
+        } else {
+            locationText = "Unable to start Location Updates - No permission granted";
+            updateLocationOnScreen();
+        }
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+    }
+
+
+    /**
+     * This method updates the location update text view on screen
+     * it executes every time a location is received
+     */
+
+
+    private void updateLocationOnScreen(){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                TextView tv = (TextView) findViewById(R.id.tv_location_update);
+                tv.setText(locationText);
+                tv =(TextView) findViewById(R.id.tv_counter);
+                tv.setText("Update #: " + String.valueOf(counter++));
+            }
+        });
+
+    }
+
+
+
 
 
 }
